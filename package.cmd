2d65b3613e54b18268a9ad3d28db693816a5ac72
Greg Kurts <gkurts@gregkurts.com>
2016-05-30 10:42:46 -0500

    bumping version up a bit

--- package.cmd
+++ package.cmd
@@ -3,8 +3,8 @@ tools\nuget.exe update -self
 if not exist package mkdir package
 if not exist package mkdir package
 if not exist package\lib mkdir package\lib
-if not exist package\lib\3.5 mkdir package\lib\3.5
+if not exist package\lib\4.6.1 mkdir package\lib\4.6.1
 
-copy JWT\bin\Release\JWT.dll package\lib\3.5\
+copy JWT\bin\Release\JWT.dll package\lib\4.6.1\
 
 tools\nuget.exe pack JWT.nuspec -BasePath package
