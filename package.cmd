545b92d6e51fcceff7253bbb224793f7f64c162a
Greg Kurts <gkurts@gregkurts.com>
2016-05-30 17:42:50 -0500

    updating nuspec and package script to reflect net461 changes

--- package.cmd
+++ package.cmd
@@ -3,8 +3,8 @@ tools\nuget.exe update -self
 if not exist package mkdir package
 if not exist package mkdir package
 if not exist package\lib mkdir package\lib
-if not exist package\lib\4.6.1 mkdir package\lib\4.6.1
+if not exist package\lib\net461 mkdir package\lib\net461
 
-copy JWT\bin\Release\JWT.dll package\lib\4.6.1\
+copy JWT\bin\Release\JWT.dll package\lib\net461
 
 tools\nuget.exe pack JWT.nuspec -BasePath package
